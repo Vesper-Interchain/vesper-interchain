7906081235963eab96dc812956e3dedd90a329c4
preetsinghmakkar <preetsinghmakkar154@gmail.com>
2026-06-09 14:31:45 +0530

    module(stablecoin) : created params.go with denom : uvusd

--- x/stablecoin/types/params.go
+++ x/stablecoin/types/params.go
@@ -1,17 +1,34 @@
 package types
 
-// NewParams creates a new Params instance.
-func NewParams() Params {
-	return Params{}
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// NewParams creates a new Params instance
+func NewParams(stableDenom string) Params {
+	return Params{
+		StableDenom: stableDenom,
+	}
 }
 
-// DefaultParams returns a default set of parameters.
+// DefaultParams returns a default set of parameters
 func DefaultParams() Params {
-	return NewParams()
+	return Params{
+		StableDenom: "uvusd",
+	}
 }
 
-// Validate validates the set of params.
+// Validate validates the params
 func (p Params) Validate() error {
+	if p.StableDenom == "" {
+		return fmt.Errorf("stable_denom cannot be empty")
+	}
+
+	if err := sdk.ValidateDenom(p.StableDenom); err != nil {
+		return fmt.Errorf("invalid stable_denom: %w", err)
+	}
 
 	return nil
 }
